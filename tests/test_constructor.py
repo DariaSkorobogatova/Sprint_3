6afc17f0bb68bc50954f6ebbdcf2fdbbcfbbdba9
Daria Skorobogatova <daria.skorobogatova@yandex.ru>
2023-08-12 16:35:26 +0300

    sprint 3_changed test_constructor, added class Constructor with methods and class PConstructorLocators with locators

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,56 +1,41 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-
 from variables import *
-from locators import *
+from fixture.authorisation import Authorisation
+from fixture.registration import Registration
+from fixture.constructor import Constructor
 
 
 class TestConstructor:
 
     def test_choose_sause_section(self, driver):
-        driver.get(login_page)
-
-        WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, enter_button)))
-
-        driver.find_element(By.XPATH, input_email).send_keys(email_for_auth)
-        driver.find_element(By.NAME, input_password).send_keys(passwd_for_auth)
-        driver.find_element(By.XPATH, enter_button).click()
-
-        WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, main_page_make_order_button)))
-
-        driver.find_element(By.XPATH, sause_section).click()
-
-        assert 'current' in driver.find_element(By.XPATH, sause_section).get_attribute('class')
+        auth = Authorisation(driver)
+        cnstr = Constructor(driver)
+        reg = Registration(driver)
+        auth.go_to_login_page()
+        reg.wait_for_enter_bt_is_clickable()
+        auth.log_in(email_for_auth, passwd_for_auth)
+        auth.wait_make_order_bt_is_clickable()
+        cnstr.click_sause_section()
+        assert 'current' in cnstr.get_attribute_sause_section()
 
     def test_choose_filling_section(self, driver):
-        driver.get(login_page)
-
-        WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, enter_button)))
-
-        driver.find_element(By.XPATH, input_email).send_keys(email_for_auth)
-        driver.find_element(By.NAME, input_password).send_keys(passwd_for_auth)
-        driver.find_element(By.XPATH, enter_button).click()
-
-        WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, main_page_make_order_button)))
-
-        driver.find_element(By.XPATH, filling_section).click()
-
-        assert 'current' in driver.find_element(By.XPATH, filling_section).get_attribute('class')
+        auth = Authorisation(driver)
+        cnstr = Constructor(driver)
+        reg = Registration(driver)
+        auth.go_to_login_page()
+        reg.wait_for_enter_bt_is_clickable()
+        auth.log_in(email_for_auth, passwd_for_auth)
+        auth.wait_make_order_bt_is_clickable()
+        cnstr.click_filling_section()
+        assert 'current' in cnstr.get_attribute_filling_section()
 
     def test_choose_bun_section(self, driver):
-        driver.get(login_page)
-
-        WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, enter_button)))
-
-        driver.find_element(By.XPATH, input_email).send_keys(email_for_auth)
-        driver.find_element(By.NAME, input_password).send_keys(passwd_for_auth)
-        driver.find_element(By.XPATH, enter_button).click()
-
-        WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.XPATH, main_page_make_order_button)))
-
-        driver.find_element(By.XPATH, filling_section).click()
-
-        driver.find_element(By.XPATH, bun_section).click()
-
-        assert 'current' in driver.find_element(By.XPATH, bun_section).get_attribute('class')
+        auth = Authorisation(driver)
+        cnstr = Constructor(driver)
+        reg = Registration(driver)
+        auth.go_to_login_page()
+        reg.wait_for_enter_bt_is_clickable()
+        auth.log_in(email_for_auth, passwd_for_auth)
+        auth.wait_make_order_bt_is_clickable()
+        cnstr.click_filling_section()
+        cnstr.click_bun_section()
+        assert 'current' in cnstr.get_attribute_bun_section()
